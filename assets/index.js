60ad439addb3d710c4b8d4718faa482117402bca
Antonio Hernández <antoniohernandez95@gmail.com>
2020-04-26 16:37:40 -0400

    Finalizada la clase de IntersectionObserver

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
 const video = document.querySelector("video");
 const buttonPlay = document.getElementById("toggle-play");
@@ -7,7 +8,7 @@ const buttonMute = document.getElementById("toggle-mute");
 
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay()],
+  plugins: [new AutoPlay(), new AutoPause()],
 });
 
 buttonPlay.onclick = () => player.togglePlay();
