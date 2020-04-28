8411a2b63e9e57e3b902703485f198bf736d852f
Antonio Hernández <antoniohernandez95@gmail.com>
2020-04-28 00:03:50 -0400

    AutoPlay y MediaPlayer convertidos a TS

--- assets/index.ts
+++ assets/index.ts
@@ -1,10 +1,10 @@
-import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./plugins/AutoPlay.js";
-import AutoPause from "./plugins/AutoPause.ts";
+import MediaPlayer from "./MediaPlayer";
+import AutoPlay from "./plugins/AutoPlay";
+import AutoPause from "./plugins/AutoPause";
 
 const video = document.querySelector("video");
-const buttonPlay = document.getElementById("toggle-play");
-const buttonMute = document.getElementById("toggle-mute");
+const buttonPlay: HTMLElement = document.getElementById("toggle-play");
+const buttonMute: HTMLElement = document.getElementById("toggle-mute");
 
 const player = new MediaPlayer({
   el: video,
