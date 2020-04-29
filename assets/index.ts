db5e4b247336eb2771d48565cd6a0cf16020a5af
Antonio Hernández <antoniohernandez95@gmail.com>
2020-04-29 02:47:24 -0400

    Agregado plugin de Ads

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from "./MediaPlayer";
 import AutoPlay from "./plugins/AutoPlay";
 import AutoPause from "./plugins/AutoPause";
+import Ads from "./plugins/Ads";
 
 const video = document.querySelector("video");
 const buttonPlay: HTMLElement = document.getElementById("toggle-play");
@@ -8,7 +9,7 @@ const buttonMute: HTMLElement = document.getElementById("toggle-mute");
 
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay(), new AutoPause()],
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
 buttonPlay.onclick = () => player.togglePlay();
